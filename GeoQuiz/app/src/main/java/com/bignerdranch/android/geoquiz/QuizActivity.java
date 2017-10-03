4471dc5e5706b3b5a8fa32162f700f8e7ca04b2a
葛帅琦 <geshuaiqi@geshuaiqideMacBook-Pro.local>
2017-10-03 22:01:03 +0800

    chapter 2 done

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,27 +2,111 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
+
+
+
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButtion;
+    private Button mLastButtion;
+    private TextView mQuestionTextView;
+
+    private static final String TAG = "QuizActivity";
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_mideast,false),
+            new Question(R.string.question_africa,false),
+            new Question(R.string.question_america,true),
+            new Question(R.string.question_asia,true),
+    };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion(){
+        Log.d(TAG,"Update called");
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+
+        Log.d(TAG,"CheckAnswer called");
+
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int  messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        } else{
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG,"onCreate(Bundle) called");
+
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+//        int question = mQuestionBank[mCurrentIndex].getTextResId();
+//        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuizActivity.this,
+//                        R.string.incorrect_toast,
+//                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -30,10 +114,45 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuizActivity.this,
+//                        R.string.correct_toast,
+//                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButtion = (Button) findViewById(R.id.next_button);
+        mNextButtion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = (mCurrentIndex + 1 + mQuestionBank.length) % mQuestionBank.length;
+//                int question = mQuestionBank[mCurrentIndex].getTextResId();
+//                mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+
+        mLastButtion = (Button) findViewById(R.id.last_button);
+        mLastButtion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
+//                int question = mQuestionBank[mCurrentIndex].getTextResId();
+//                mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+
+        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1 + mQuestionBank.length) % mQuestionBank.length;
+//                int question = mQuestionBank[mCurrentIndex].getTextResId();
+//                mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
     }
 }
